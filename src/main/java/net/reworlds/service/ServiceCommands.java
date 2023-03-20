266b79ff792bba73578d8ada625155a883c21f29
Statuxia <statuxia@ya.ru>
2023-03-20 23:11:15 +0300

    Добавлена команда user, состоящая из player/id, stats, punish Добавлена команда start (функция help) Добавлена фановая команда coin (мутит участника группы на X секунд в случае его проигрыша) Удалены команды player, id, stats, punish Оптимизированы имеющиеся команды.

--- src/main/java/net/reworlds/service/ServiceCommands.java
+++ src/main/java/net/reworlds/service/ServiceCommands.java
@@ -2,7 +2,11 @@ package net.reworlds.service;
 
 import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Chat;
+import com.pengrad.telegrambot.model.ChatPermissions;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.User;
+import com.pengrad.telegrambot.request.RestrictChatMember;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import net.reworlds.cache.Cache;
@@ -12,7 +16,7 @@ import net.reworlds.config.CommandText;
 import net.reworlds.utils.MessageUtils;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ServiceCommands implements Service {
     private final TelegramBot bot;
@@ -51,71 +55,24 @@ public class ServiceCommands implements Service {
 
     @Override
     public void metrics() {
-        String message = CommandText.metricsMessage;
         Metrics metrics = Cache.getMetrics();
-        message = message.replace("$update-time", metrics.getUpdateTime().toString());
-        message = message.replace("$online", "" + metrics.getOnline());
-        message = message.replace("$tps", metrics.getTps().toString());
-
-        execute(MessageUtils.buildMessage(update, message));
+        execute(MessageUtils.buildMessage(update, String.format(CommandText.metricsMessage, metrics.getUpdateTime(), metrics.getOnline(), metrics.getTps())));
     }
 
     @Override
-    public void player() {
+    public void user() {
         if (args.length < 2) {
-            execute(MessageUtils.buildMessage(update, CommandText.noPlayerMessage));
+            execute(MessageUtils.buildMessage(update, CommandText.noUserMessage));
             return;
         }
 
-        Player player = Cache.getPlayer(args[1], Player.Type.NAME);
+        Player player = Cache.getPlayer(args[1]);
         if (player.getName() == null) {
-            String message = CommandText.unknownPlayerMessage;
-            message = message.replace("$player_name", args[1]);
-            execute(MessageUtils.buildMessage(update, message));
-            return;
-        }
-        String message = CommandText.playerMessage;
-        message = message.replace("$rank", player.getRank());
-        message = message.replace("$player_name", player.getName());
-        message = message.replace("$id", "" + player.getId());
-        message = message.replace("$discord_id", "" + player.getDiscordID());
-        message = message.replace("$uuid", player.getUUID());
-        message = message.replace("$first_seen", player.getFirstSeen());
-        message = message.replace("$last_seen", player.getLastSeen());
-        message = message.replace("$playtime", player.getPlayTime());
-        if (player.isOnline()) {
-            message = message.replace("Оффлайн", "Онлайн");
-        }
-        execute(MessageUtils.buildMessage(update, message));
-    }
-
-    @Override
-    public void id() {
-        if (args.length < 2) {
-            execute(MessageUtils.buildMessage(update, CommandText.noIDMessage));
+            execute(MessageUtils.buildMessage(update, String.format(CommandText.unknownUserMessage, args[1])));
             return;
         }
 
-        Player player = Cache.getPlayer(args[1], Player.Type.ID);
-        if (player.getName() == null) {
-            String message = CommandText.unknownIDMessage;
-            message = message.replace("$id", args[1]);
-            execute(MessageUtils.buildMessage(update, message));
-            return;
-        }
-        String message = CommandText.playerMessage;
-        message = message.replace("$rank", player.getRank());
-        message = message.replace("$player_name", player.getName());
-        message = message.replace("$id", "" + player.getId());
-        message = message.replace("$discord_id", "" + player.getDiscordID());
-        message = message.replace("$uuid", player.getUUID());
-        message = message.replace("$first_seen", player.getFirstSeen());
-        message = message.replace("$last_seen", player.getLastSeen());
-        message = message.replace("$playtime", player.getPlayTime());
-        if (player.isOnline()) {
-            message = message.replace("Оффлайн", "Онлайн");
-        }
-        execute(MessageUtils.buildMessage(update, message));
+        execute(MessageUtils.buildMessage(update, player.toString()));
     }
 
     @Override
@@ -125,114 +82,58 @@ public class ServiceCommands implements Service {
             return;
         }
 
-        Player player = Cache.getPlayer(args[1], Player.Type.ALL);
+        Player player = Cache.getPlayer(args[1]);
         if (player.getName() == null) {
-            String message = CommandText.unknownSkinMessage;
-            message = message.replace("$player", args[1]);
-            execute(MessageUtils.buildMessage(update, message));
+            execute(MessageUtils.buildMessage(update, String.format(CommandText.unknownSkinMessage, args[1])));
             return;
         }
-        String message = CommandText.skinMessage;
-        message = message.replace("$rank", player.getRank());
-        message = message.replace("$player_name", player.getName());
-        message = message.replace("$id", "" + player.getId());
-        message = message.replace("$discord_id", "" + player.getDiscordID());
-        message = message.replace("$uuid", player.getUUID());
-        execute(MessageUtils.buildMessage(update, message));
+        execute(MessageUtils.buildMessage(update, String.format(CommandText.skinMessage, player.getRank(), player.getName(), player.getId(),
+                player.getDiscordID(), player.getUUID())));
 
     }
 
     @Override
-    public void punish() {
+    public void coin() {
         if (args.length < 2) {
-            execute(MessageUtils.buildMessage(update, CommandText.noPunishMessage));
+            execute(MessageUtils.buildMessage(update, CommandText.noCoinMessage));
             return;
         }
 
-        Player player = Cache.getPlayer(args[1], Player.Type.ALL);
-        if (player.getName() == null) {
-            String message = CommandText.unknownPunishMessage;
-            message = message.replace("$player", args[1]);
-            execute(MessageUtils.buildMessage(update, message));
-            return;
-        }
-        String message = CommandText.punishMessage;
-        message = message.replace("$rank", player.getRank());
-        message = message.replace("$player_name", player.getName());
-        message = message.replace("$id", "" + player.getId());
-        message = message.replace("$discord_id", "" + player.getDiscordID());
-        message = message.replace("$uuid", player.getUUID());
-
-        if (player.getBans().size() > 0) {
-            String[] banMessage = message.split("\n └ Этот игрок может играть на сервере.");
-
-            List<Player.Ban> bans = player.getBans();
-            for (int i = 0; i < bans.size(); i++) {
-                Player.Ban ban = bans.get(i);
-                if (i + 1 == bans.size()) {
-                    banMessage[0] += "\n ├ Модератор: " + ban.getModerator() +
-                            "\n ├ Причина: " + ban.getReason() +
-                            "\n ├ Выдан: " + ban.getIssued() +
-                            "\n └ Истечет: " + ban.getExpire();
-                    message = banMessage[0] + banMessage[1];
-                    break;
-                }
-                banMessage[0] += "\n ├ Модератор: " + ban.getModerator() +
-                        "\n ├ Причина: " + ban.getReason() +
-                        "\n ├ Выдан: " + ban.getIssued() +
-                        "\n ├ Истечет: " + ban.getExpire();
+        int time;
+        try {
+            time = Integer.parseInt(args[1]);
+            if (time <= 30 || time >= 31622400) {
+                execute(MessageUtils.buildMessage(update, CommandText.unknownCoinMessage));
+                return;
             }
-        }
-
-        if (player.getMutes().size() > 0) {
-            message = message.replace("\n └ Этот игрок может разговаривать в игровом чате.", "");
-            List<Player.Mute> mutes = player.getMutes();
-            for (int i = 0; i < mutes.size(); i++) {
-                Player.Mute mute = mutes.get(i);
-                if (i + 1 == mutes.size()) {
-                    message += "\n ├ Модератор: " + mute.getModerator() +
-                            "\n ├ Причина: " + mute.getReason() +
-                            "\n ├ Выдан: " + mute.getIssued() +
-                            "\n └ Истечет: " + mute.getExpire();
-                    break;
-                }
-                message += "\n ├ Модератор: " + mute.getModerator() +
-                        "\n ├ Причина: " + mute.getReason() +
-                        "\n ├ Выдан: " + mute.getIssued() +
-                        "\n ├ Истечет: " + mute.getExpire();
-            }
-        }
-        execute(MessageUtils.buildMessage(update, message));
-    }
-
-    @Override
-    public void stats() {
-        if (args.length < 2) {
-            execute(MessageUtils.buildMessage(update, CommandText.noStatsMessage));
+        } catch (NumberFormatException e) {
+            execute(MessageUtils.buildMessage(update, CommandText.unknownCoinMessage));
             return;
         }
 
-        Player player = Cache.getPlayer(args[1], Player.Type.ALL);
-        if (player.getName() == null) {
-            String message = CommandText.unknownStatsMessage;
-            message = message.replace("player", args[1]);
-            execute(MessageUtils.buildMessage(update, message));
+        User user = update.message().from();
+        Chat chat = update.message().chat();
+
+        if (user.id().equals(chat.id())) {
+            execute(MessageUtils.buildMessage(update, CommandText.noCoinChatMessage));
             return;
         }
-        String message = CommandText.statsMessage;
-        message = message.replace("$rank", player.getRank());
-        message = message.replace("$player_name", player.getName());
-        message = message.replace("$id", "" + player.getId());
-        message = message.replace("$discord_id", "" + player.getDiscordID());
-        message = message.replace("$uuid", player.getUUID());
 
-        message = message.replace("$deaths", "" + player.getStats().getDeaths());
-        message = message.replace("$kills", "" + player.getStats().getKills());
-        message = message.replace("$mob_kills", "" + player.getStats().getMobKills());
-        message = message.replace("$broken_blocks", "" + player.getStats().getBrokenBlocks());
-        message = message.replace("$placedBlocks", "" + player.getStats().getPlacedBlocks());
-        message = message.replace("$advancements", "" + player.getStats().getAdvancements());
-        execute(MessageUtils.buildMessage(update, message));
+        int random = ThreadLocalRandom.current().nextInt(2);
 
+        if (random == 1) {
+            execute(MessageUtils.buildMessage(update, String.format(CommandText.coinMessage,
+                    "Орел", "Вы победили!")));
+        } else {
+            execute(MessageUtils.buildMessage(update, String.format(CommandText.coinMessage,
+                    "Решка", "Вы проиграли и будете замучены на " + time + " секунд!")));
+            RestrictChatMember restrict = new RestrictChatMember(
+                    chat.id(),
+                    user.id(),
+                    new ChatPermissions().canSendMessages(false)
+            );
+            restrict = restrict.untilDate((int) (System.currentTimeMillis() / 1000L) + time);
+            bot.execute(restrict);
+        }
     }
 }
