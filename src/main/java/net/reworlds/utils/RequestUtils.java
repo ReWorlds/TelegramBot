71a32b4b0c22f1c40eaf7ffdf1957fcca1eed7a8
Statuxia <statuxia@ya.ru>
2023-03-20 23:05:42 +0300

    HttpClient вынесен в главный класс за ненадобностью создавать его при каждом вызове RequestUtils

--- src/main/java/net/reworlds/utils/RequestUtils.java
+++ src/main/java/net/reworlds/utils/RequestUtils.java
@@ -1,20 +1,20 @@
 package net.reworlds.utils;
 
+import net.reworlds.Bot;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class RequestUtils {
     public static JSONObject getJSON(String url) {
         try {
-            HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpResponse<String> response = Bot.getClient().send(request, HttpResponse.BodyHandlers.ofString());
             return new JSONObject(response.body());
+
         } catch (IOException | InterruptedException e) {
             return null;
         }
