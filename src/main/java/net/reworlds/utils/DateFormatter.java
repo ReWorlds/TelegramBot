d5f2183c35ec88cf3d03380bd54ee4d00e87cbc9
Statuxia <statuxia@ya.ru>
2023-03-28 00:09:33 +0300

    вынос общих переменных. добавление функции подсчета разницы между датами.

--- src/main/java/net/reworlds/utils/DateFormatter.java
+++ src/main/java/net/reworlds/utils/DateFormatter.java
@@ -1,22 +1,26 @@
 package net.reworlds.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class DateFormatter {
+
+    public static final String unknownDate = "неизвестно";
+    public static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+
     public static String formatDate(Date date) {
-        String pattern = "dd.MM.yyyy HH:mm:ss";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
         String stringFormat = simpleDateFormat.format(date);
         if (stringFormat.contains("1970")) {
-            return "неизвестно";
+            return unknownDate;
         }
         return stringFormat;
     }
 
     public static String formatMillis(long millis) {
-       return durationToStr(Duration.ofMillis(millis));
+        return durationToStr(Duration.ofMillis(millis));
     }
 
     public static String formatSeconds(long seconds) {
@@ -37,5 +41,29 @@ public class DateFormatter {
                     duration.toMinutesPart(), duration.toSecondsPart());
         }
         return String.format("%d сек.", duration.toSecondsPart());
+    }
+
+
+    public static long difference(String date) throws ParseException {
+        if (unknownDate.equals(date)) {
+            return 8;
+        }
+        Date firstDate = simpleDateFormat.parse(date);
+        Date secondDate = new Date();
+        return difference(firstDate, secondDate);
+    }
+
+    public static long difference(String date, String date2) throws ParseException {
+        if (unknownDate.equals(date) || unknownDate.equals(date2)) {
+            return 8;
+        }
+        Date firstDate = simpleDateFormat.parse(date);
+        Date secondDate = simpleDateFormat.parse(date2);
+        return difference(firstDate, secondDate);
+    }
+
+    public static long difference(Date date1, Date date2) {
+        long diffInMillies = Math.abs(date2.getTime() - date1.getTime());
+        return TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
     }
 }
