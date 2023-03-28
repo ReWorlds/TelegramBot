5bdc9d2c4dc71274321b9a1931028bc282d5c233
Statuxia <statuxia@ya.ru>
2023-03-28 19:00:08 +0300

    Adding ConnectionPool

---
+++ src/main/java/net/reworlds/database/ConnectionPool.java
@@ -0,0 +1,42 @@
+package net.reworlds.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import net.reworlds.Bot;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionPool {
+
+    public static String setAccountString;
+    public static String getAccountString;
+    private static final HikariConfig config = new HikariConfig();
+    private static HikariDataSource source;
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            config.setJdbcUrl("jdbc:mysql://%s/%s"
+                    .formatted(Bot.getJson().getString("database-host"), Bot.getJson().getString("database-name")));
+            config.setUsername(Bot.getJson().getString("database-user"));
+            config.setPassword(Bot.getJson().getString("database-password"));
+
+
+            String table = Bot.getJson().getString("database-table");
+            setAccountString = "INSERT INTO %s (id, name) VALUES (?, ?) ON DUPLICATE KEY UPDATE name = ?;".formatted(table);
+            getAccountString = "SELECT * FROM %s WHERE id = ? LIMIT 1".formatted(table);
+
+            source = new HikariDataSource(config);
+        } catch (ClassNotFoundException e) {
+            Bot.getLogger().warn(e, e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return source.getConnection();
+    }
+
+    private ConnectionPool() {
+    }
+}
