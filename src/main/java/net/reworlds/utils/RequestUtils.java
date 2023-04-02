0671c7d88eca9700e580d905dba04930ef6c467b
Statuxia <statuxia@ya.ru>
2023-04-02 23:57:25 +0300

    Добавлено время ожидания

--- src/main/java/net/reworlds/utils/RequestUtils.java
+++ src/main/java/net/reworlds/utils/RequestUtils.java
@@ -7,11 +7,12 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 
 public class RequestUtils {
     public static JSONObject getJSON(String url) {
         try {
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).timeout(Duration.ofSeconds(1)).build();
             HttpResponse<String> response = Bot.getClient().send(request, HttpResponse.BodyHandlers.ofString());
             if (response.statusCode() != 200) {
                 return null;
