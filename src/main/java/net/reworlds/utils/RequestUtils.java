cca809e7b1718db8947de380e22b5ddf7babcdbf
Statuxia <statuxia@ya.ru>
2023-04-03 20:21:49 +0300

    Добавлена документация для объекта getJSON

--- src/main/java/net/reworlds/utils/RequestUtils.java
+++ src/main/java/net/reworlds/utils/RequestUtils.java
@@ -10,17 +10,18 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 
 public class RequestUtils {
-    public static JSONObject getJSON(String url) {
-        try {
+    /**
+     * Позволяет получать <code>JSONObject</code> с REST API страниц.
+     *
+     * @param url ссылка на страницу, с которой необъодимо взять json.
+     * @return JSONObject, в случае успешного (200) запроса или null, в случае, если запрос не удался.
+     */
+    public static JSONObject getJSON(String url) throws IOException, InterruptedException {
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).timeout(Duration.ofSeconds(1)).build();
             HttpResponse<String> response = Bot.getClient().send(request, HttpResponse.BodyHandlers.ofString());
             if (response.statusCode() != 200) {
                 return null;
             }
             return new JSONObject(response.body());
-
-        } catch (IOException | InterruptedException e) {
-            return null;
-        }
     }
 }
