f74ebf8ca41719c7c29c7956f48a9cb58584eee3
Statuxia <statuxia@ya.ru>
2023-04-03 20:59:27 +0300

    Добавлена документация и аннотации, Упрощен метод durationToStr.

--- src/main/java/net/reworlds/utils/DateFormatter.java
+++ src/main/java/net/reworlds/utils/DateFormatter.java
@@ -1,5 +1,8 @@
 package net.reworlds.utils;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
@@ -11,7 +14,15 @@ public class DateFormatter {
     public static final String unknownDate = "неизвестно";
     public static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 
-    public static String formatDate(Date date) {
+    /**
+     * Позволяет отформатировать дату по паттерну "<code>dd.MM.yyyy HH:mm:ss</code>".
+     * В случае, если в строке присутствует 1970, возвращает "<code>неизвестно</code>".
+     *
+     * @param date дата на форматирование.
+     * @return отформатированная дата в виде строки или "<code>неизвестно</code>", в случае,
+     * если в строке присутствует 1970.
+     */
+    public static @NotNull String formatDate(@NotNull Date date) {
         String stringFormat = simpleDateFormat.format(date);
         if (stringFormat.contains("1970")) {
             return unknownDate;
@@ -19,49 +30,98 @@ public class DateFormatter {
         return stringFormat;
     }
 
-    public static String formatMillis(long millis) {
+
+    /**
+     * Позволяет отформатировать миллисекунды в строку в формате количества дней, часов, минут и секунд.
+     * Возможные возвращаемые значения:<br>
+     * "<code>%d дн. %d ч. %d мин. %d сек.</code>"<br>
+     * "<code>%d ч. %d мин. %d сек.</code>"<br>
+     * "<code>%d мин. %d сек.</code>"<br>
+     * "<code>%d сек.</code>"
+     *
+     * @param millis миллисекунды, которые необходимо отформатировать.
+     * @return отформатированная строка.
+     */
+    public static @NotNull String formatMillis(long millis) {
         return durationToStr(Duration.ofMillis(millis));
     }
 
-    public static String formatSeconds(long seconds) {
+    /**
+     * Позволяет отформатировать секунды в строку в формате количества дней, часов, минут и секунд.
+     * Возможные возвращаемые значения:<br>
+     * "<code>%d дн. %d ч. %d мин. %d сек.</code>"<br>
+     * "<code>%d ч. %d мин. %d сек.</code>"<br>
+     * "<code>%d мин. %d сек.</code>"<br>
+     * "<code>%d сек.</code>"
+     *
+     * @param seconds секунды, которые необходимо отформатировать.
+     * @return отформатированная строка.
+     */
+    public static @NotNull String formatSeconds(long seconds) {
         return durationToStr(Duration.ofSeconds(seconds));
     }
 
-    private static String durationToStr(Duration duration) {
-        if (duration.toDaysPart() != 0) {
-            return String.format("%d дн. %d ч. %d мин. %d сек.",
-                    duration.toDaysPart(), duration.toHoursPart(), duration.toMinutesPart(), duration.toSecondsPart());
-        }
-        if (duration.toHoursPart() != 0) {
-            return String.format("%d ч. %d мин. %d сек.",
-                    duration.toHoursPart(), duration.toMinutesPart(), duration.toSecondsPart());
-        }
-        if (duration.toMinutesPart() != 0) {
-            return String.format("%d мин. %d сек.",
-                    duration.toMinutesPart(), duration.toSecondsPart());
-        }
-        return String.format("%d сек.", duration.toSecondsPart());
+    /**
+     * Позволяет отформатировать время в строку в формате количества дней, часов, минут и секунд.
+     * Возможные возвращаемые значения:<br>
+     * "<code>%d дн. %d ч. %d мин. %d сек.</code>"<br>
+     * "<code>%d ч. %d мин. %d сек.</code>"<br>
+     * "<code>%d мин. %d сек.</code>"<br>
+     * "<code>%d сек.</code>"
+     *
+     * @param duration значение времени.
+     * @return отформатированная строка.
+     */
+    private static @NotNull String durationToStr(@Nullable Duration duration) {
+        return duration == null ? "0 сек."
+                : duration.toDaysPart() != 0 ? String.format("%d дн. %d ч. %d мин. %d сек.",
+                duration.toDaysPart(), duration.toHoursPart(), duration.toMinutesPart(), duration.toSecondsPart())
+                : duration.toHoursPart() != 0 ? String.format("%d ч. %d мин. %d сек.",
+                duration.toHoursPart(), duration.toMinutesPart(), duration.toSecondsPart())
+                : duration.toMinutesPart() != 0 ? String.format("%d мин. %d сек.", duration.toMinutesPart(),
+                duration.toSecondsPart())
+                : String.format("%d сек.", duration.toSecondsPart());
     }
 
-
-    public static long difference(String date) throws ParseException {
+    /**
+     * Позволяет получить разницу между передаваемой датой и нынешней в днях.
+     *
+     * @param date строка в формате "<code>dd.MM.yyyy HH:mm:ss</code>".
+     * @return разницу в днях или -1, если строка - "<code>неизвестно</code>"
+     */
+    public static long difference(@NotNull String date) throws ParseException {
         if (unknownDate.equals(date)) {
-            return 8;
+            return -1;
         }
         Date firstDate = simpleDateFormat.parse(date);
         Date secondDate = new Date();
         return difference(firstDate, secondDate);
     }
 
+    /**
+     * Позволяет получить разницу между передаваемыми датами в днях.
+     *
+     * @param date  строка в формате "<code>dd.MM.yyyy HH:mm:ss</code>".
+     * @param date2 строка в формате "<code>dd.MM.yyyy HH:mm:ss</code>".
+     * @return разницу в днях или -1, если строка - "<code>неизвестно</code>".
+     */
     public static long difference(String date, String date2) throws ParseException {
         if (unknownDate.equals(date) || unknownDate.equals(date2)) {
-            return 8;
+            return -1;
         }
         Date firstDate = simpleDateFormat.parse(date);
         Date secondDate = simpleDateFormat.parse(date2);
         return difference(firstDate, secondDate);
     }
 
+    /**
+     * Позволяет получить разницу между передаваемыми датами в днях.
+     *
+     * @param date1 объект класса <code>Date</code>.
+     * @param date2 объект класса <code>Date</code>.
+     * @return разницу в днях или -1, если строка - "<code>неизвестно</code>".
+     * @see Date
+     */
     public static long difference(Date date1, Date date2) {
         long diffInMillies = Math.abs(date2.getTime() - date1.getTime());
         return TimeUnit.DAYS.convert(diffInMillies, TimeUnit.MILLISECONDS);
