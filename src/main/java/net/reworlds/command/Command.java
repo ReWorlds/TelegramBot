55832132d945c123c6b9d41310577178b4a2406f
Statuxia <statuxia@ya.ru>
2023-04-03 21:45:14 +0300

    Перенесен в папку command, добавлена документация.

--- src/main/java/net/reworlds/command/Command.java
+++ src/main/java/net/reworlds/command/Command.java
@@ -1,13 +1,18 @@
-package net.reworlds.service;
+package net.reworlds.command;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Аннотация для сбора методов в <code>CommandDispatcher</code>.
+ * Позволяет кроме команды собирать и псевдонимы (aliases) команд.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Command {
     String value() default "";
+
     String[] aliases() default {};
 }
